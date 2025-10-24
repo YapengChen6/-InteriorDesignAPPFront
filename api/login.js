bf0a9bbbb004e9f53b5299603cddd96e0a85631f
周星阳 <2054359250@qq.com>
2025-10-24 10:23:35 +0800

    大更新

--- api/login.js
+++ api/login.js
@@ -1,3 +1,4 @@
+import { userInfo } from 'os'
 import request from '@/utils/request'
 
 // 手机号登录
@@ -29,15 +30,22 @@ export function sendCode(phoneNumber) {
 // 获取用户信息
 export function getUserInfo() {
   return request({
-    url: '/api/user/info',
+    url: '/api/users/profile',
     method: 'get'
   })
 }
-
+// 更改用户信息
+export function updateUserInfo() {
+  return request({
+    url: '/api/users/profile',
+    method: 'put',
+	  data: data
+  })
+}
 // 获取用户路由
 export function getRouters() {
   return request({
-    url: '/api/user/routers',
+    url: '/api/users/routers',
     method: 'get'
   })
 }
@@ -45,7 +53,7 @@ export function getRouters() {
 // 退出登录 - 修改后的版本
 export function logout() {
   return request({
-    url: '/logout',  // 修正路径
+    url: '/api/auth/logout',  // 修正路径
     method: 'post'
     // 移除 token 参数，让拦截器自动添加 Authorization header
   })
@@ -74,3 +82,4 @@ export function getCodeImg() {
     timeout: 20000
   })
 }
+
--- pages/mine/info/edit.vue
+++ pages/mine/info/edit.vue
@@ -8,12 +8,8 @@
         <uni-forms-item label="手机号码" name="phonenumber">
           <uni-easyinput v-model="user.phonenumber" placeholder="请输入手机号码" />
         </uni-forms-item>
-        <uni-forms-item label="邮箱" name="email">
-          <uni-easyinput v-model="user.email" placeholder="请输入邮箱" />
-        </uni-forms-item>
-        <uni-forms-item label="性别" name="sex" required>
-          <uni-data-checkbox v-model="user.sex" :localdata="sexs" />
-        </uni-forms-item>
+        
+        
       </uni-forms>
       <button type="primary" @click="submit">提交</button>
     </view>
@@ -33,13 +29,7 @@
           email: "",
           sex: ""
         },
-        sexs: [{
-          text: '男',
-          value: "0"
-        }, {
-          text: '女',
-          value: "1"
-        }],
+        
         rules: {
           nickName: {
             rules: [{
@@ -56,15 +46,7 @@
               errorMessage: '请输入正确的手机号码'
             }]
           },
-          email: {
-            rules: [{
-              required: true,
-              errorMessage: '邮箱地址不能为空'
-            }, {
-              format: 'email',
-              errorMessage: '请输入正确的邮箱地址'
-            }]
-          }
+          
         }
       }
     },
